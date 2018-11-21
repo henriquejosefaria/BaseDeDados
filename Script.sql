1403fd4d5f19d272fa8fcb29828e53c998d99b08
Naovoltei <miguel_ratuinhu@hotmail.com>
2018-11-21 14:15:34 +0000

    Add files via upload

---
+++ Script.sql
@@ -0,0 +1,17 @@
+INSERT INTO contacto
+	(idContacto,NrTelemovel,NrTelemovel2,Email)
+	VALUES
+		(1,965499765,NULL,'filipe1_cunha@hotmail.com'),
+        (2,911457895,937652981,'henrique_faria@hotmail.com'),
+        (3,916150165,NULL,'miguel_ratuinhu@gmail.com');
+        
+INSERT INTO morada
+	(idMorada,Rua,Localidade,codigoPostal)
+    VALUES
+		(1,'Rua dos peoes','Vila Verde','4730-490'),
+        (2,'Rua das alminhas','Braga','4700-005'),
+        (3,'Rua das prezes','Esposende','4740-007');
+        
+        
+        
+INSERT INTO Cliente
