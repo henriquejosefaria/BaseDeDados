9b67e6f82272ae91ce70fa388be63375f1b5f8de
Henrique Faria <henriquejosefaria@gmail.com>
2018-11-22 19:37:57 +0000

    povoamentos

---
+++ EquipamentosPovoamento.sql
@@ -0,0 +1,22 @@
+USE Ginasio;
+
+INSERT INTO Equipamento(idEquipamento,Nome,Descricao)
+    VALUES (1,'Banco','Multi Exercício'),
+                   (2,'Banco','Multi Exercício'),
+                   (3,'Banco','Multi Exercício'),
+                   (4,'Multi Press Rack','Peito'),
+                   (5,'Multi Press Rack','Peito'),
+                   (6,'Pulley Machine','MultiExercício'),
+                   (7,'Pulley Machine','MultiExercício'),
+                   (8,'Dumbbel Set','Multi Exercicio'),
+                   (9,'Dumbbel Set','Multi Exercicio'),
+                   (10,'Passadeira Corrida Eletrica','Resistência'),
+                   (11,'Passadeira Corrida Eletrica','Resistência'),
+                   (12,'Passadeira Corrida Eletrica','Resistência'),
+                   (13,'Passadeira Corrida Eletrica','Resistência'),
+                   (14,'Passadeira Corrida Autopropulsada','Resistência'),
+                   (15,'Passadeira Corrida Autopropulsada','Resistência'),
+                   (16,'Prensa','Pernas'),
+                   (17,'Pull Down Powerline','Costas'),
+                   (18,'Pull Down Powerline','Costas');
+                   
