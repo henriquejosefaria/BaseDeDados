ce80bf52caa8176cb9271318113c18370e40e082
Naovoltei <miguel_ratuinhu@hotmail.com>
2018-11-23 19:46:32 +0000

    Add files via upload

--- Script.sql
+++ Script.sql
@@ -75,7 +75,7 @@ INSERT INTO Cliente
 		('Alvaro Campos','M',24 , 20, 20, 21.5, 71.2, 1.86, 485867545,null);
         
 INSERT INTO contactofuncionario
-	(NrTelemovel,NrTelemovel2,Email)
+	(idContacto,NrTelemovel,NrTelemovel2,Email)
 	VALUES
 		(1,967541987,NULL,'stanilipastarini@outlook.com'),
         (2,96999666,928855888,'marcopolo@hotmail.com'),
@@ -87,7 +87,7 @@ INSERT INTO contactofuncionario
         (8,967541324,NULL,'move-itmove-it@outlook.com');
         
 INSERT INTO moradafuncionario
-	(Rua,Localidade,codigoPostal)
+	(idMorada,Rua,Localidade,codigoPostal)
     VALUES
 		(1,'Rua de Pousada','Gualtar','4710-049'),
         (2,'Rua 1º de Maio','Pedralva','4715-473'),
@@ -100,7 +100,7 @@ INSERT INTO moradafuncionario
 
 
 INSERT INTO Funcionario
-	(Nome,Idade,Sexo,Cargo,idContacto,idMorada)
+	(idFuncionario,Nome,Idade,Sexo,Cargo,idContacto,idMorada)
     VALUES
 		(1,'Josef Stalini',45,'M','Empregado de Limpeza',1,1),
         (2,'Genghis Khan',34,'M','Personal Traineer',2,2),
@@ -112,7 +112,7 @@ INSERT INTO Funcionario
         (8,'King Julian Move-it Move-it',26,'M','Rececionista',8,8);
         
 INSERT INTO servico
-	(Descricao,Preco)
+	(idServico,Descricao,Preco)
     VALUES
 		(1,'Natação,Passe diario',5),
         (2,'Natação,Passe mensal',30),
@@ -176,22 +176,63 @@ INSERT INTO Exercicio
 INSERT INTO planoexercicios
 	(Cliente,Exercicio,nSeries,nRepeticoes)
     VALUES
-		(1,3,15,3);
+		(1,3,15,3),
+        (2,3,15,3),
+        (5,3,10,3),
+        (1,2,15,3),
+        (1,5,12,3);
+        
         
 INSERT INTO prestaservico
 	(idServico,idFuncionario,DataInicio)
     VALUES
-		(1,3,'2018/04/20'),
-        (6,6,'2018/01/20'),
-        (5,5,'2017/12/20');
+		(1,3,'2018/01/05'),
+		(2,3,'2018/01/05'),
+		(5,2,'2018/01/5'),
+		(5,5,'2018/01/05'),
+		(6,6,'2018/01/05'),
+		(7,6,'2018/01/05');
+        
+        
         
 INSERT INTO subscreve
 	(idCliente,idServico,DataInicio)
     VALUES
-		(1,1,'2018/04/20'),
+		(1,2,'2018/04/20'),
         (1,7,'2018/04/20'),
         (1,9,'2018/04/20'),
-        (2,2,'2018/05/21');
+        (2,1,'2018/05/21'),
+        (3,4,'2018/05/21'),
+        (3,8,'2018/05/21'),
+        (4,2,'2018/06/01'),
+        (4,7,'2018/06/23'),
+        (5,5,'2018/11/02'),
+        (6,7,'2018/10/11'),
+        (7,4,'2018/10/01'),
+        (8,4,'2018/11/01'),
+        (8,8,'2018/11/20'),
+        (9,1,'2018/10/20'),
+        (9,2,'2018/11/01'),
+        (10,3,'2018/05/06'),
+        (10,3,'2018/05/07'),
+        (10,3,'2018/05/08'),
+        (10,3,'2018/05/09'),
+        (10,4,'2018/05/10'),
+        (11,4,'2018/02/07'),
+        (11,2,'2018/02/08'),
+        (12,3,'2018/05/10'),
+        (13,1,'2018/08/10'),
+        (13,5,'2018/08/10'),
+        (14,7,'2018/05/07'),
+        (15,6,'2018/03/07'),
+        (15,4,'2018/03/07'),
+        (16,3,'2018/09/07'),
+        (17,8,'2018/09/10'),
+        (18,5,'2018/05/01'),
+        (18,5,'2018/06/07'),
+        (19,4,'2018/05/07'),
+        (20,4,'2018/10/07');
+        
         
 INSERT INTO ExercicioEquipamento 
 	(idExercicio, idEquipamento)
@@ -220,32 +261,36 @@ INSERT INTO ExercicioEquipamento
 		(2,16),
 		(17,17);
                  
-        
-        
-INSERT INTO Fatura 
-	(idFatura,ContribuinteGinasio ,Data,Descricao,Valor,idCliente,Desconto,Funcionario)
+ INSERT INTO Fatura 
+	(idFatura,ContribuinteGinasio ,Data,Descricao,Valor,Funcionario,idCliente,Desconto )
 	VALUES 
-		(1,111111111,STR_TO_DATE( "5/01/2018",  "%d/%m/%Y" ),'Natação, Mensal & Ginásio, Mensal, PersonalTrainer',150,1,30,1),
-		(2,111111111,STR_TO_DATE( "5/01/2018",  "%d/%m/%Y" ),'Ginásio, Mensal & Nutrição, ida única',55,2,5,1),
-		(3,111111111,STR_TO_DATE( "5/01/2018",  "%d/%m/%Y" ),'Ginasio, Mensal, PersonalTrainer',120,3,20,1),
-		(4,111111111,STR_TO_DATE( "5/01/2018",  "%d/%m/%Y" ),'Natação, Mensal & Ginásio, Mensal, PersonalTrainer',150,4,30,1),
-		(5,111111111,STR_TO_DATE( "6/01/2018",  "%d/%m/%Y" ),'Natação, Mensal & Sauna, ida única',33,5,10,1),
-		(6,111111111,STR_TO_DATE( "6/01/2018",  "%d/%m/%Y" ),'Natação, Mensal & Nutrição, 2x',80,6,15,1),
-		(7,111111111,STR_TO_DATE( "6/01/2018",  "%d/%m/%Y" ),'Ginásio, Mensal, PersonalTrainer',120,7,23.20,1),
-		(8,111111111,STR_TO_DATE( "7/01/2018",  "%d/%m/%Y" ),'Ginásio, Mensal, PersonalTrainer',120,8,23.20,1),
-		(9,111111111,STR_TO_DATE( "7/01/2018",  "%d/%m/%Y" ),'Ginásio, Mensal, PersonalTrainer & Nutrição, ida única',150,9,20,1),
-		(10,111111111,STR_TO_DATE( "7/01/2018",  "%d/%m/%Y" ),'Ginásio, Mensal & Nutrição, 2x',75,10,10,1),
-		(11,111111111,STR_TO_DATE( "7/01/2018",  "%d/%m/%Y" ),'Ginásio, Mensal & Nutrição, ida única',55,11,5,1),
-		(12,111111111,STR_TO_DATE( "7/01/2018",  "%d/%m/%Y" ),'Natação, Mensal & Ginásio, Mensal',55,12,15,1),
-		(13,111111111,STR_TO_DATE( "8/01/2018",  "%d/%m/%Y" ),'Ginásio, Mensal & Nutrição, ida única',55,13,5,1),
-		(14,111111111,STR_TO_DATE( "8/01/2018",  "%d/%m/%Y" ),'Natação, Mensal & Ginásio, Mensal, PersonalTrainer',130,14,30,1),
-		(15,111111111,STR_TO_DATE( "8/01/2018",  "%d/%m/%Y" ),'Ginásio, Mensal ',25,15,5,1),
-		(16,111111111,STR_TO_DATE( "8/01/2018",  "%d/%m/%Y" ),'Ginásio, Mensal ',25,16,5,1),
-		(17,111111111,STR_TO_DATE( "8/01/2018",  "%d/%m/%Y" ),'Ginásio,Passe Diário & Sauna, Passe Diário',6,17,0,1),
-		(18,111111111,STR_TO_DATE( "8/01/2018",  "%d/%m/%Y" ),'Ginásio, Mensal ',25,18,5,1),
-		(19,111111111,STR_TO_DATE( "8/01/2018",  "%d/%m/%Y" ),'Natação, Mensal & Ginásio, Mensal, PersonalTrainer',150,19,30,1),
-		(20,111111111,STR_TO_DATE( "8/01/2018",  "%d/%m/%Y" ),'Ginásio Mensal',25,20,5,1);
+				(1,111111111,'2018/01/05','Natação, Mensal & Ginásio, Mensal, PersonalTrainer',150,7,1,30),
+                (2,111111111,STR_TO_DATE( "5/01/2018",  "%d/%m/%Y" ),'Ginásio, Mensal & Nutrição, ida única',55,7,2,5),
+                (3,111111111,STR_TO_DATE( "5/01/2018",  "%d/%m/%Y" ),'Ginasio, Mensal, PersonalTrainer',120,8,3,20),
+                (4,111111111,STR_TO_DATE( "5/01/2018",  "%d/%m/%Y" ),'Natação, Mensal & Ginásio, Mensal, PersonalTrainer',150,8,4,30),
+                (5,111111111,STR_TO_DATE( "6/01/2018",  "%d/%m/%Y" ),'Natação, Mensal & Sauna, ida única',33,7,5,10),
+                (6,111111111,STR_TO_DATE( "6/01/2018",  "%d/%m/%Y" ),'Natação, Mensal & Nutrição, 2x',80,7,6,15),
+                (7,111111111,STR_TO_DATE( "6/01/2018",  "%d/%m/%Y" ),'Ginásio, Mensal, PersonalTrainer',120,7,7,23.20),
+                (8,111111111,STR_TO_DATE( "7/01/2018",  "%d/%m/%Y" ),'Ginásio, Mensal, PersonalTrainer',120,8,8,23.20),
+                (9,111111111,STR_TO_DATE( "7/01/2018",  "%d/%m/%Y" ),'Ginásio, Mensal, PersonalTrainer & Nutrição, ida única',150,8,9,20),
+                (10,111111111,STR_TO_DATE( "7/01/2018",  "%d/%m/%Y" ),'Ginásio, Mensal & Nutrição, 2x',75,8,10,10),
+                (11,111111111,STR_TO_DATE( "7/01/2018",  "%d/%m/%Y" ),'Ginásio, Mensal & Nutrição, ida única',55,8,11,5),
+                (12,111111111,STR_TO_DATE( "7/01/2018",  "%d/%m/%Y" ),'Natação, Mensal & Ginásio, Mensal',55,8,12,15),
+                (13,111111111,STR_TO_DATE( "8/01/2018",  "%d/%m/%Y" ),'Ginásio, Mensal & Nutrição, ida única',55,8,13,5),
+                (14,111111111,STR_TO_DATE( "8/01/2018",  "%d/%m/%Y" ),'Natação, Mensal & Ginásio, Mensal, PersonalTrainer',130,8,14,30),
+                (15,111111111,STR_TO_DATE( "8/01/2018",  "%d/%m/%Y" ),'Ginásio, Mensal ',25,8,15,5),
+                (16,111111111,STR_TO_DATE( "8/01/2018",  "%d/%m/%Y" ),'Ginásio, Mensal ',25,8,16,5),
+                (17,111111111,STR_TO_DATE( "8/01/2018",  "%d/%m/%Y" ),'Ginásio,Passe Diário & Sauna, Passe Diário',6,7,17,0),
+                (18,111111111,STR_TO_DATE( "8/01/2018",  "%d/%m/%Y" ),'Ginásio, Mensal ',25,7,18,5),
+                (19,111111111,STR_TO_DATE( "8/01/2018",  "%d/%m/%Y" ),'Natação, Mensal & Ginásio, Mensal, PersonalTrainer',150,7,19,30),
+                (20,111111111,STR_TO_DATE( "8/01/2018",  "%d/%m/%Y" ),'Ginásio Mensal',25,7,20,5);
+ 
+
+ -- // Maneira de apagar tabela toda// --
+-- DELETE FROM Fatura WHERE idFatura != 387
+      
         
+
 INSERT INTO servicofatura
 	(idFatura,idServico)
     VALUES
@@ -283,13 +328,4 @@ INSERT INTO servicofatura
         (19,5),
         (20,4);
 
-SELECT SUM(Valor) FROM Fatura as F
-	WHERE MONTH(F.Data)=01 AND YEAR(F.DATA)=2018; 
-    
-SELECT idCliente,Nome FROM Cliente as C
-	INNER JOIN planoexercicios as P on P.exercicio='Prensa';
-    
-SELECT Descricao,nome FROM Cliente as C
-	INNER JOIN subscreve as S on C.idCliente=S.idCliente
-    INNER JOIN servico as Z on S.Idservico=Z.idservico
-    WHERE (C.idCliente=1);
+
