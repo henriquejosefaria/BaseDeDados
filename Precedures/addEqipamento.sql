8ba3397a5cafb309f59591cb7f02aad1f733ee03
Henrique Faria <henriquejosefaria@gmail.com>
2018-11-25 22:55:46 +0000

    adds

--- Precedures/addEqipamento.sql
+++
@@ -1,5 +0,0 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `addEquipamento`(in Nome VARCHAR(45), in Descricao VARCHAR(75))
-BEGIN
-    INSERT INTO Equipamento (Nome,Descricao)
-        VALUES (Nome,Descricao);
-END
--- Precedures/addFuncionario.sql
+++ Precedures/addFuncionario.sql
@@ -1,4 +1,4 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `addFuncionario`(in idFuncionario INT(11), in nome VARCHAR(75),in idade INT(11), in sexo VARCHAR(45),in cargo VARCHAR(45), in idContacto INT(11),in nrTelomovel INT(11), in nrTelemovel2 INT(11), in email VARCHAR(75), in idMorada INT(11), in rua VARCHAR(75), in localidade VARCHAR(75), in codigoPostal VARCHAR(75))
+CREATE DEFINER=`root`@`localhost` PROCEDURE `addFuncionario`( in nome VARCHAR(75),in idade INT(11), in sexo VARCHAR(45),in cargo VARCHAR(45), in nrTelomovel INT(11), in nrTelemovel2 INT(11), in email VARCHAR(75), in rua VARCHAR(75), in localidade VARCHAR(75), in codigoPostal VARCHAR(75))
 BEGIN
    DECLARE EXIT HANDLER FOR SQLEXCEPTION
    BEGIN
@@ -6,13 +6,13 @@ BEGIN
       RESIGNAL;
    END;
    START TRANSACTION;
-   INSERT INTO ContactoFuncionario (idContacto,NrTelemovel,NrTelemovel2,Email)
-           VALUE (idContacto,nrTelomovel,nrTelemovel2,email);
+   INSERT INTO ContactoFuncionario (NrTelemovel,NrTelemovel2,Email)
+           VALUE (nrTelomovel,nrTelemovel2,email);
            
-   INSERT INTO MoradaFuncionario (idMorada,rua,localidade,codigoPostal)
-          VALUE (idMorada,rua,localidade,codigoPostal);
+   INSERT INTO MoradaFuncionario (rua,localidade,codigoPostal)
+          VALUE (irua,localidade,codigoPostal);
           
-   INSERT INTO Funcionario (idFuncionario,Nome,Idade,Sexo,Cargo,idContacto,idMorada)
-          VALUE (idFuncionario,nome,idade,sexo,cargo,idContacto,idMorada);
+   INSERT INTO Funcionario (Nome,Idade,Sexo,Cargo,idContacto,idMorada)
+          VALUE (nome,idade,sexo,cargo,idContacto,idMorada);
    COMMIT;
 END
