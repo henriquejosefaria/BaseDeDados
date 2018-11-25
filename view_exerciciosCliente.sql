99fe7489d99874f284f21d03814ae77f954176a9
RyzonPT <36732127+RyzonPT@users.noreply.github.com>
2018-11-25 14:01:43 +0000

    Delete view_exerciciosCliente.sql

--- view_exerciciosCliente.sql
+++
@@ -1,5 +0,0 @@
-CREATE VIEW `view_exerciciosCliente` AS
-
-SELECT C.Nome AS NomeCliente, E.Descricao AS NomeExercicio, PE.nSeries,PE.nRepeticoes FROM PlanoExercicios AS PE
-   INNER JOIN Exercicio AS E ON E.idExercicio = PE.Exercicio
-   INNER JOIN Cliente AS C ON C.idCLiente = PE.Cliente;
