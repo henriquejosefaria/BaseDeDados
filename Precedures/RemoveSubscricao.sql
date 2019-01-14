e7a9bb405fc0aff09c0043a3899dac345f223e03
Naovoltei <miguel_ratuinhu@hotmail.com>
2019-01-14 21:39:13 +0000

    Delete RemoveSubscricao.sql

--- Precedures/RemoveSubscricao.sql
+++
@@ -1,5 +0,0 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `RemoveSubscricao`(in servicoID INT, in clienteID INT, in dataID DATE)
-BEGIN
-	DELETE FROM subscreve
-		where (idCliente = clienteID && IdServico = servicoID && DataSubscricao = dataID);
-END
