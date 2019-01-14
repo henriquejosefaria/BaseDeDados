a9e57d131666691d3d4a8816ee4ba608aef1d62e
Naovoltei <miguel_ratuinhu@hotmail.com>
2019-01-14 21:38:47 +0000

    Delete RemovePlanoExercicio.sql

--- Precedures/RemovePlanoExercicio.sql
+++
@@ -1,5 +0,0 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `RemovePlanoExercicio`(in clienteID INT, exercicioID INT)
-BEGIN
-	DELETE FROM planoexercicios
-		where (Cliente = clienteID && Exercicio = exercicioID);
-END
