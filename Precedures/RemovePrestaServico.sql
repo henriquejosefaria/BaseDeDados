bfb91f44ad287e921b8e0b79dbba1932585c2e8f
Naovoltei <miguel_ratuinhu@hotmail.com>
2019-01-14 21:38:56 +0000

    Delete RemovePrestaServico.sql

--- Precedures/RemovePrestaServico.sql
+++
@@ -1,5 +0,0 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `RemovePrestaServico`(in funcionarioID INT, in servicoID INT)
-BEGIN
-	DELETE FROM prestaservico
-		where idServico = servicoID && idFuncionario = funcionarioID;
-END
