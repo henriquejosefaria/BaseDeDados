05dec970be941ff88ab0e0e86be7842fdfb52a99
Naovoltei <miguel_ratuinhu@hotmail.com>
2019-01-15 22:16:33 +0000

    Add files via upload

--- Precedures/UpdateServico.sql
+++ Precedures/UpdateServico.sql
@@ -2,7 +2,7 @@ DELIMITER $$
 CREATE PROCEDURE `updateservico` (in id int(11))
 BEGIN
 UPDATE servico as s
-SET s.estado= IF(s.estado = 'A', 'I','A')
+SET s.estado= IF(s.estado = 'A', 'I','A'),s.UptoDate='N'
 WHERE (s.idServico=id) ;
 END$$
 
--- Precedures/addPrestaServico.sql
+++ Precedures/addPrestaServico.sql
@@ -1,8 +1,8 @@
 DELIMITER $$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `addPrestaServico`(in idFuncionario INT(11), in idServico INT(11), in DataInicio DATE)
+CREATE DEFINER=`root`@`localhost` PROCEDURE `addPrestaServico`(in idServ INT(11),in idFunc INT(11),  in DataInicio DATE)
 BEGIN
     INSERT INTO PrestaServico(idServico,idFuncionario,DataInicio)
-         VALUE(idFuncionario,idServico,DataInicio);
+         VALUES(idServ,idFunc,DataInicio);
 END $$
 
 DELIMITER ;
