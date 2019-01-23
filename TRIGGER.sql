6642830fea212dd1a251431de5ccb14bc793e963
Naovoltei <miguel_ratuinhu@hotmail.com>
2019-01-23 22:18:39 +0000

    Add files via upload

--- TRIGGER.sql
+++ TRIGGER.sql
@@ -1,12 +1,25 @@
 DELIMITER $$
-CREATE TRIGGER updatefuncionario
+CREATE TRIGGER updatefuncionario1
 AFTER UPDATE ON servico FOR EACH ROW
 begin
            UPDATE funcionario as f
            INNER JOIN prestaservico as ps on ps.idFuncionario=f.idFuncionario
            INNER JOIN servico on  ps.idServico=NEW.idServico
-		   SET f.UptoDate =IF(f.UptoDate = 'R', 'S','N');
+		   SET f.UptoDate = CASE
+    WHEN f.UptoDate ='R' THEN 'R'
+    WHEN f.UptoDate ='S' THEN 'N'
+    ELSE f.UptoDate ='N'
+    END;
+    
+END;
+$$
+DELIMITER ;
 
+DELIMITER $$
+CREATE TRIGGER updateservico
+BEFORE UPDATE ON servico FOR EACH ROW
+begin
+           SET new.UptoDate =IF(new.UptoDate = 'R', 'S','N');
 END;
 $$
 DELIMITER ;
@@ -17,7 +30,7 @@ AFTER INSERT ON prestaservico FOR EACH ROW
 begin
            UPDATE funcionario as f
            INNER JOIN prestaservico as ps on new.idFuncionario=f.idFuncionario
-           SET f.UptoDate =IF(f.UptoDate = 'R', 'S','N');
+           SET f.UptoDate ='N';
 END;
 $$
 DELIMITER ;
@@ -29,7 +42,7 @@ AFTER INSERT ON planoexercicios FOR EACH ROW
 begin
            UPDATE cliente as c
            INNER JOIN planoexercicios as pe on new.Cliente=c.idCliente
-		   SET c.UptoDate =IF(c.UptoDate = 'R', 'S','N');
+		   SET c.UptoDate ='N';
 
 END;
 $$
@@ -41,7 +54,7 @@ AFTER UPDATE ON planoexercicios FOR EACH ROW
 begin
            UPDATE cliente as c
            INNER JOIN planoexercicios on c.idCliente=NEW.Cliente
-           SET c.UptoDate =IF(c.UptoDate = 'R', 'S','N');
+           SET c.UptoDate = c.UptoDate = 'N';
 END;
 $$
 DELIMITER ;
@@ -53,7 +66,11 @@ AFTER UPDATE ON fatura FOR EACH ROW
 begin
            UPDATE cliente as c
            INNER JOIN fatura on c.idCliente=NEW.idCliente
-           SET c.UptoDate =IF(c.UptoDate = 'R', 'S','N');
+           SET c.UptoDate = CASE
+    WHEN c.UptoDate ='R' THEN 'R'
+    WHEN c.UptoDate ='S' THEN 'N'
+    ELSE c.UptoDate ='N'
+    END;
 END;
 $$
 DELIMITER ;
@@ -69,5 +86,29 @@ END;
 $$
 DELIMITER ;
 
+DELIMITER $$
+CREATE TRIGGER updatecliente
+BEFORE UPDATE ON cliente FOR EACH ROW
+begin
+           SET new.UptoDate =IF(new.UptoDate = 'R', 'S','N');
+END;
+$$
+DELIMITER ;
 
+DELIMITER $$
+CREATE TRIGGER updatefatura
+BEFORE UPDATE ON fatura FOR EACH ROW
+begin
+           SET new.UptoDate =IF(new.UptoDate = 'R', 'S','N');
+END;
+$$
+DELIMITER ;
 
+DELIMITER $$
+CREATE TRIGGER updatefuncionario
+BEFORE UPDATE ON funcionario FOR EACH ROW
+begin
+           SET new.UptoDate =IF(new.UptoDate = 'R', 'S','N');
+END;
+$$
+DELIMITER ;
